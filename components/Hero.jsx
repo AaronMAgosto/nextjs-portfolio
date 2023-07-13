4ea95017acfea8f8e24f8a893d244a85b45defe4
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-07-13 10:50:48 -0400

    making changes to the hero buttons and adding resume

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,22 +1,47 @@
-export default function Hero () {
+export default function Hero() {
+
+  const profileUrl =
+    "https://www.bocacode.com/candidates/software-engineer/aaron-agosto";
 
   return (
     <section className="text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-      <img className="object-cover object-center rounded" alt="hero" src="../../images/aaron1.jpg" />
-    </div>
-    <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"> Aaron Agosto <br/> Software Engineer
-        <br className="hidden lg:inline-block"readymade gluten />
-      </h1>
-      <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-      <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-        <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="../../images/aaron1.jpg"
+          />
+        </div>
+        <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            {" "}
+            Aaron Agosto <br /> Software Engineer
+            <br className="hidden lg:inline-block" readymade gluten />
+          </h1>
+          <p className="mb-8 leading-relaxed text-white">
+            As an enthusiastic and motivated software engineer, I am driven by
+            continuous learning and exploring various aspects of software
+            development. I have a particular interest in front-end and back-end
+            development, Cloud Computing, DevSecOps, and project management.
+            Currently seeking new opportunities to gain valuable experience and
+            contribute to the field of software engineering.
+          </p>
+          <div className="flex justify-center">
+            {/* <a
+              href={profileUrl}
+              className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+              Boca Code Portfolio
+            </a> */}
+
+            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" >See Resume </button>
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
-  )
+    </section>
+  );
 }
+
