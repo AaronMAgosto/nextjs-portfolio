9f7d264811f4a24eab0493d110f8259bf517c170
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-07-14 11:32:36 -0400

    final touches before first deployment to vercel

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -68,16 +68,24 @@ export default function Hero() {
               </a>
               <a
                 href="https://soflohoneyholes.com/"
-                className="ml-3 text-titanium-500 inline-flex"
+                className="ml-3 text-white inline-flex"
                 target="_blank"
                 rel="noreferrer"
               >
-                <img
-                  src="/images/link.png"
-                  height="30"
-                  width="30"
-                  alt="link to project"
-                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                  />
+                </svg>
               </a>
 
               <a
@@ -100,16 +108,11 @@ export default function Hero() {
                     d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
                   />
                 </svg>
-                </a>
+              </a>
             </span>
           </div>
         </div>
       </div>
     </section>
   );
-}
-{
-  /* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-</svg> */
 }
